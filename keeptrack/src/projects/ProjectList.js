66c7e2d0dc09148df80ba16ba12b1566909efa84
AlexandreGaultier <alexandre.gaultier@epsi.fr>
2024-02-05 22:19:03 +0100

    Editing Project works

--- keeptrack/src/projects/ProjectList.js
+++ keeptrack/src/projects/ProjectList.js
@@ -4,28 +4,39 @@ import { Project } from './Project';
 import ProjectCard from './ProjectCard';
 import ProjectForm from './ProjectForm';
 
-function ProjectList({ projects }) {
-    const [projectBeingEdited, setProjectBeingEdited] = useState({});
-    const handleEdit = (project) => {
-        setProjectBeingEdited(project);
-    };
-    return (
-        <div className="row">
-            {projects.map((project) => (
-                <div key={project.id} className="cols-sm">
-                    {project === projectBeingEdited ? (
-                        <ProjectForm />
-                    ) : (
-                        <ProjectCard project={project} onEdit={handleEdit} />
-                    )}
-                </div>
-            ))}
+function ProjectList({ projects, onSave }) {
+  const [projectBeingEdited, setProjectBeingEdited] = useState({});
+
+  const handleEdit = (project) => {
+    setProjectBeingEdited(project);
+  };
+
+  const cancelEditing = () => {
+    setProjectBeingEdited({});
+  };
+
+  return (
+    <div className="row">
+      {projects.map((project) => (
+        <div key={project.id} className="cols-sm">
+          {project === projectBeingEdited ? (
+            <ProjectForm
+              project={project}
+              onSave={onSave}
+              onCancel={cancelEditing}
+            />
+          ) : (
+            <ProjectCard project={project} onEdit={handleEdit} />
+          )}
         </div>
-    );
+      ))}
+    </div>
+  );
 }
 
 ProjectList.propTypes = {
-    projects: PropTypes.arrayOf(PropTypes.instanceOf(Project)).isRequired
+  projects: PropTypes.arrayOf(PropTypes.instanceOf(Project)).isRequired,
+  onSave: PropTypes.func.isRequired,
 };
 
 export default ProjectList;
--- keeptrack/src/projects/ProjectsPage.js
+++ keeptrack/src/projects/ProjectsPage.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import { MOCK_PROJECTS } from './MockProjects';
 import ProjectList from './ProjectList';
 function ProjectsPage() {
+    const [projects, setProjects] = useState(MOCK_PROJECTS);
+
+    const saveProject = (project) => {
+        let updatedProjects = projects.map((p) => {
+            return p.id === project.id ? project : p;
+        });
+        setProjects(updatedProjects);
+    };
     return (
         <>
             <h1>Projects</h1>
-            <ProjectList projects={MOCK_PROJECTS} />
+            <ProjectList onSave={saveProject} projects={projects} />
         </>
     );
 }
